27626b13e8fc5ef57e927cebbcfe6bc4675be056
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2017-12-20 19:41:22 +0300

    Code cleanup

--- post-processor.go
+++ post-processor.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/hashicorp/packer/common"
-	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
 	"github.com/hashicorp/packer/packer/plugin"
-	"github.com/hashicorp/packer/template/interpolate"
 	"os"
 	"strings"
+	"fmt"
 )
 
 const TeamcityVersionEnvVar = "TEAMCITY_VERSION"
@@ -20,69 +16,6 @@ var AmazonBuilderIds = []string {
 	"mitchellh.amazon.chroot",
 }
 
-type Config struct {
-	common.PackerConfig `mapstructure:",squash"`
-
-	OutputPath string `mapstructure:"output"`
-	StripPath  bool   `mapstructure:"strip_path"`
-	ctx        interpolate.Context
-}
-
-type PostProcessor struct {
-	config Config
-}
-
-type ManifestFile struct {
-	Builds      []Artifact `json:"builds"`
-	LastRunUUID string     `json:"last_run_uuid"`
-}
-
-func Contains(vs []string, t string) bool {
-	for _, v := range vs {
-		if v == t {
-			return true
-		}
-	}
-	return false
-}
-
-func (p *PostProcessor) Configure(raws ...interface{}) error {
-	err := config.Decode(&p.config, &config.DecodeOpts{
-		Interpolate:        true,
-		InterpolateContext: &p.config.ctx,
-		InterpolateFilter: &interpolate.RenderFilter{
-			Exclude: []string{},
-		},
-	}, raws...)
-	if err != nil {
-		return err
-	}
-
-	if p.config.OutputPath == "" {
-		p.config.OutputPath = "packer-manifest.json"
-	}
-
-	if err = interpolate.Validate(p.config.OutputPath, &p.config.ctx); err != nil {
-		return fmt.Errorf("Error parsing target template: %s", err)
-	}
-
-	return nil
-}
-
-func (p *PostProcessor) PostProcess(ui packer.Ui, source packer.Artifact) (packer.Artifact, bool, error) {
-	if os.Getenv(TeamcityVersionEnvVar) != "" {
-		if Contains(AmazonBuilderIds, source.BuilderId())  {
-			s := strings.Split(source.Id(), ":")
-			region, ami := s[0], s[1] // TODO: several AMIs
-			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.aws.region' value='%v']", region))
-			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.aws.ami' value='%v']", ami))
-		} else {
-			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.id' value='%v']", source.Id()))
-		}
-	}
-	return source, true, nil
-}
-
 func main() {
 	server, err := plugin.Server()
 	if err != nil {
@@ -90,4 +23,34 @@ func main() {
 	}
 	server.RegisterPostProcessor(new(PostProcessor))
 	server.Serve()
+}
+
+type PostProcessor struct {
+}
+
+func (p *PostProcessor) Configure(raws ...interface{}) error {
+	return nil
+}
+
+func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
+	if os.Getenv(TeamcityVersionEnvVar) != "" {
+		if contains(AmazonBuilderIds, artifact.BuilderId())  {
+			s := strings.Split(artifact.Id(), ":")
+			region, ami := s[0], s[1] // TODO: several AMIs
+			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.aws.region' value='%v']", region))
+			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.aws.ami' value='%v']", ami))
+		} else {
+			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.id' value='%v']", artifact.Id()))
+		}
+	}
+	return artifact, true, nil
+}
+
+func contains(slice []string, value string) bool {
+	for _, element := range slice {
+		if element == value {
+			return true
+		}
+	}
+	return false
 }
