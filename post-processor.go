3ed2dbe6bcdcbe106419790ccb39c2870c1740a8
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2017-12-21 00:33:53 +0300

    Update cloud agent image on TeamCity server

--- post-processor.go
+++ post-processor.go
@@ -8,10 +8,14 @@ import (
 	"os"
 	"strings"
 	"fmt"
+	"net/http"
+	"bytes"
+	"errors"
 )
 
 const TeamcityVersionEnvVar = "TEAMCITY_VERSION"
-var AmazonBuilderIds = []string {
+
+var AmazonBuilderIds = []string{
 	"mitchellh.amazonebs",
 	"mitchellh.amazon.ebssurrogate",
 	"mitchellh.amazon.instance",
@@ -28,7 +32,18 @@ func main() {
 }
 
 type PostProcessor struct {
-	config common.PackerConfig
+	config Config
+}
+
+type Config struct {
+	common.PackerConfig `mapstructure:",squash"`
+
+	TeamCityUrl     string `mapstructure:"teamcity_url"`
+	Username        string `mapstructure:"username"`
+	Password        string `mapstructure:"password"`
+	ProjectId       string `mapstructure:"project_id"`
+	CustomImageName string `mapstructure:"custom_image_name"`
+	AgentName       string `mapstructure:"agent_name"`
 }
 
 func (p *PostProcessor) Configure(raws ...interface{}) error {
@@ -37,20 +52,74 @@ func (p *PostProcessor) Configure(raws ...interface{}) error {
 		return err
 	}
 
+	errs := new(packer.MultiError)
+	if p.config.TeamCityUrl != "" {
+		if p.config.Username == "" {
+			errs = packer.MultiErrorAppend(errs, fmt.Errorf("username is required"))
+		}
+		if p.config.Password == "" {
+			errs = packer.MultiErrorAppend(errs, fmt.Errorf("password is required"))
+		}
+		if p.config.ProjectId == "" {
+			errs = packer.MultiErrorAppend(errs, fmt.Errorf("project_id is required"))
+		}
+		if p.config.CustomImageName == "" {
+			errs = packer.MultiErrorAppend(errs, fmt.Errorf("custom_image_name is required"))
+		}
+		if p.config.AgentName == "" {
+			errs = packer.MultiErrorAppend(errs, fmt.Errorf("agent_name is required"))
+		}
+	}
+
+	if len(errs.Errors) > 0 {
+		return errs
+	}
+
 	return nil
 }
 
 func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
 	if os.Getenv(TeamcityVersionEnvVar) != "" {
-		if contains(AmazonBuilderIds, artifact.BuilderId())  {
+		if contains(AmazonBuilderIds, artifact.BuilderId()) {
 			s := strings.Split(artifact.Id(), ":")
-			region, ami := s[0], s[1] // TODO: several AMIs
+			region, ami := s[0], s[1]
 			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.%v.aws.region' value='%v']", p.config.PackerBuildName, region))
 			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.%v.aws.ami' value='%v']", p.config.PackerBuildName, ami))
 		} else {
 			ui.Message(fmt.Sprintf("##teamcity[setParameter name='packer.artifact.%v.id' value='%v']", p.config.PackerBuildName, artifact.Id()))
 		}
 	}
+
+	if p.config.TeamCityUrl != "" {
+		url := fmt.Sprintf(
+			"%v/httpAuth/app/rest/projects/id:%v/projectFeatures/type:CloudImage,property(name:source-id,value:%v)/properties/sourceVmName",
+			strings.TrimRight(p.config.TeamCityUrl, "/"),
+			p.config.ProjectId,
+			p.config.CustomImageName,
+		)
+		body := bytes.NewBufferString(p.config.AgentName)
+
+		c := &http.Client{}
+		req, err := http.NewRequest("PUT", url, body)
+		if err != nil {
+			return artifact, true, err
+		}
+		req.Header.Add("Content-Type", "text/plain")
+		req.SetBasicAuth(p.config.Username, p.config.Password)
+
+		resp, err := c.Do(req)
+		if err != nil {
+			return artifact, true, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			return artifact, true, errors.New(fmt.Sprintf("Error updating a cloud profile: %v", resp.Status))
+		}
+
+		ui.Message(fmt.Sprintf("Cloud agent image '%v' is switched to image '%v'", p.config.CustomImageName, p.config.AgentName))
+	}
+
 	return artifact, true, nil
 }
 
