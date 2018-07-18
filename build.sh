0282cfc7175d90dc2a6c98b2e35e769c434421db
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2018-07-18 21:39:39 +0300

    dep instead of Glide, Packer 1.2.5, go 1.10.3

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -eux
+#!/bin/sh
+set -eux
 
-glide install -v
+dep ensure
 export CGO_ENABLED=0
 export GOARCH=amd64
 mkdir -p bin
+rm -f bin/*
 
 GOOS=darwin  go build -o bin/packer-post-processor-teamcity.macos
 GOOS=linux   go build -o bin/packer-post-processor-teamcity.linux
